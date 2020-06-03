0f41d841ec9134faa890773ff7a3493a4ac08551
Andrius Martini <andrius.martini1994@gmail.com>
2020-06-03 00:04:26 -0300

    Init the application's database and API

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,10 @@
 import express from 'express'
+import routes from './routes'
+import path from 'path'
 
 const app = express()
-
-app.get('/users', (request, response) => {
-  response.json({'hello': 'world'})
-})
+app.use(express.json())
+app.use(routes)
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')))
 
 app.listen(3333)
