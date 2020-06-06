654e4d8cf543141a53f0aabce7c2a55788a563a7
Andrius Martini <andrius.martini1994@gmail.com>
2020-06-05 23:42:29 -0300

    Add input validations

--- server/src/controllers/PointsController.ts
+++ server/src/controllers/PointsController.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from 'express'
 import knex from '../database/connection'
 
+
 class PointsController {
   async index(request: Request, response: Response) {
     const { city, state, items } = request.query
